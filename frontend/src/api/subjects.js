7fbe2637fda1ffc283c58d88e714c1181af5dcef
KasakAnand07 <kasakanand07@gmail.com>
2025-11-07 22:34:22 +0530

    Fixed API URL to connect frontend with Render backend

--- frontend/src/api/subjects.js
+++ frontend/src/api/subjects.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/subjects";
+const API_URL = `${process.env.REACT_APP_API_URL}/subjects`;
 
 export const getSubjects = async () => {
   const { data } = await axios.get(API_URL);
